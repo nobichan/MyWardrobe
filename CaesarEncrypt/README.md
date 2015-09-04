342c0d837ceb1206196a3a9ed6a786c52283a0e9
ISHIKAWA Sachihiro <sachihiro@gmail.com>
2015-09-04 22:32:06 +0900

    add encrypted mode.
    暗号化もできるようにした。

--- CaesarEncrypt/README.md
+++ CaesarEncrypt/README.md
@@ -1,14 +1,23 @@
 caesar
 ======
 
-Niantic Project のビデオに出てくるカエサル暗号の復号プログラム
+Niantic Project のビデオに出てくるカエサル暗号の暗号/復号プログラム
 作ってみた
 
 使い方
 ------
-
+暗号化
 ```
-$ ./caesar < encoded.txt
-This led to a disturbing period.Was I a ...
+$ ./caesar -e 2 < plain.txt
++2 Vjku ngf vq c fkuvwtdkpi rgtkqf.
+....
 $
 ```
+復号化
+```
+$ ./caesar -d < encoded.txt
+This led to a disturbing period.
+....
+$
+```
+
--- CaesarEncrypt/caesar.c
+++ CaesarEncrypt/caesar.c
@@ -1,28 +1,77 @@
 #include <stdio.h>
-
-main(int argc, char *arcv[])
+#define	DEC	0	// DECODE mode
+#define	ENC	1	// ENCODE mode
+extern void usage(void);
+main(int argc, char *argv[])
 {
 	int c = 0;
 	int i = 0;
 	int shift = 0;
 	char token[BUFSIZ] = "";
-	while ((c = getchar()) != ' ') {
-		token[i++] = c;
+	int conv = DEC;	// mode
+	if (argc == 1 || argc > 3) {
+		usage();
+		exit(-1);
 	}
-	shift = atoi(&(token[1]));
-	if (token[0] == '-') {
-		shift *= -1;
-	}
-	while ((c = getchar()) != EOF) {
-		if (('A' <= c && c <= 'Z') ||
-			('a' <= c && c <= 'z')) {
-			c -= shift;
-			if (('Z' < c && c < 'a') ||
-				(c < 'A')){
-				c += ('Z' - 'A' + 1);
-			}
+	if (argv[1][0] == '-') {
+		if (argv[1][1] == 'd') {
+			conv = DEC;
 		}
-		putchar(c);
+		else if (argv[1][1] == 'e') {
+			conv = ENC;
+			shift = atoi(argv[2]);
+		}
 	}
+	else {
+		usage();
+		exit(-1);
+	}
+	if (conv == DEC) {
+		while ((c = getchar()) != ' ') {
+			token[i++] = c;
+		}
+		shift = atoi(&(token[1]));
+		if (token[0] == '-') {
+			shift *= -1;
+		}
+		while ((c = getchar()) != EOF) {
+			if (('A' <= c && c <= 'Z') ||
+				('a' <= c && c <= 'z')) {
+				c -= shift;
+				if (('Z' < c && c < 'a') ||
+					(c < 'A')){
+					c += ('Z' - 'A' + 1);
+				}
+			}
+			putchar(c);
+		}
+	}
+	else if (conv == ENC) {
+		int a = atoi(argv[2]);
+		if (a >= 0) {
+			printf("+%d ", a);
+		}
+		else {
+			printf("%d ", a);
+		}
+		while ((c = getchar()) != EOF) {
+			if (('A' <= c && c <= 'Z') ||
+				('a' <= c && c <= 'z')) {
+				c += shift;
+				if (( 'Z' < c && c < 'a') ||
+					(c < 'A')) {
+					c += ('Z' - 'A' + 1);
+				}
+			}
+			putchar(c);
+		}
+	}
+}
+void usage(void)
+{
+	printf("caesar <parameter> < textfile\n");
+	printf("caesar -d < textfile     : decode text file.\n");
+	printf("caesar -e num < textfile : encode text file.\n");
+	printf("       num : shift num.\n");
 }
 
